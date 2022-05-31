666c267861c60242630a27459a24bf6a7f9e1c2c
chabymanow <chabymanow@gmail.com>
2022-05-31 20:42:57 +0100

    Updates

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <MainNav />
+  <h1 class="text-9xs mt-1 text-ellipsis">Chaby Manow</h1>
+  <img alt="Vue logo" src="@/assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+import MainNav from "@/components/MainNav.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    MainNav,
   },
 };
 </script>
--- src/playground.js
+++ src/playground.js
@@ -1,4 +1,4 @@
 const fruits = ["apple", "banana", "orange"];
 const vegetable = ["cucumber", "radish"];
 
-console.log([...fruits, ...vegetable]);
+// console.log([...fruits, ...vegetable]);
